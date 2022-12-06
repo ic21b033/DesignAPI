a922d3a950b51368abdd04cdf668c8c80f1a61f4
ic21b033 <ic21b033@technikum-wien.at>
2022-12-06 18:08:54 +0100

    Spring Boot #1

---
+++ src/main/java/fhtw/at/DesignAPI/DesignApiApplication.java
@@ -0,0 +1,13 @@
+package fhtw.at.DesignAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/at/DesignAPI/DesignApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.at.DesignAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
